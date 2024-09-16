e029c25e4455da733e4577461d86c3737d85b089
K-D-Piper <kirsten.piper@mscoding.org>
2024-09-16 15:18:27 -0500

    Created Game Board
    Added background colors. In javascript added event listener so changes color when clicked

--- apps.js
+++ apps.js
@@ -1,2 +1,42 @@
-let test = "work?"
-console.log(test)
+//naming variables START
+
+//Getting with Query Selector 
+
+let hits = document.querySelectorAll('.hit');
+let misses= document.querySelectorAll('.miss');
+let clearId = document.getElementById("clearID")
+//naming variables END
+
+//clear START
+
+clearId.addEventListener("click", () => {
+    button.clearid.reset();
+})
+
+//clear END (doesn't work yet)
+
+
+//Functions START
+
+hits.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log("hit")
+        alert("You HIT my battleship!")
+        button.style.color = 'green'
+    })
+});
+
+misses.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log("miss")
+        alert("HA! MISSED!")
+        button.style.color = 'red'
+    })
+});
+
+//Functions END
+
+//Ramdomizing Squares START
+
+
+
